32e60b9f8fc9130b063ff9095d52c3d228fcd23c
Gabriela Yan <gabriela.maldonado.yan@gmail.com>
2022-03-15 11:46:53 -0600

    Personalizar encabezado POST método createSnippet

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Write([]byte("Hello from Snippetbox"))
+
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
@@ -20,11 +21,20 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", 405)
+		return
+	}
+
 	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
+
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
@@ -32,4 +42,5 @@ func main() {
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
+
 }
