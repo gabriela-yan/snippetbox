911d68cc3fa8df762d52ac9469156691d0a7c298
Gabriela Yan <gabriela.maldonado.yan@gmail.com>
2022-04-04 16:37:55 -0500

    Parseando los datos del formulario

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -53,9 +53,15 @@ func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 
-	title := "O snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi Issa"
-	expires := "7"
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+	expires := r.PostForm.Get("expires")
 
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
