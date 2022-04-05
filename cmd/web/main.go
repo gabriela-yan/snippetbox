b15144399de3cd4c8f66677f601193b10a3c68ab
Gabriela Yan <gabriela.maldonado.yan@gmail.com>
2022-04-05 16:57:25 -0500

    Generando certificado TLS autofirmado

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -49,6 +49,7 @@ func main() {
 
 	session := sessions.New([]byte(*secret))
 	session.Lifetime = 12 * time.Hour
+	session.Secure = true
 
 	app := &application{
 		errorLog:      errorLog,
@@ -65,7 +66,7 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	errorLog.Fatal(err)
 
 }
